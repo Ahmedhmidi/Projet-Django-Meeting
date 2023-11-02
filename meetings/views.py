51ded911bb33d4a66aa41220c3d5a40452fe102e
unknown <ahmedformed@gmail.com>
2023-11-02 11:50:05 +0100

    DjangoApp

---
+++ meetings/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import get_object_or_404, render
+
+from meetings.models import Meeting
+
+
+# Create your views here.
+
+def detail(request, id):
+    meeting = Meeting.objects.get(pk=id)
+    return render(request, "meetings/detail.html", {"meeting": meeting})
+
+def detail1(request, id):
+    meeting = get_object_or_404(Meeting, id)
+    return render(request, "meetings/detail1.html", {"meeting": meeting})
